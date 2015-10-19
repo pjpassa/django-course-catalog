3c90adfd664c5eec334c3f9c58c2073e3e58d116
PJ <pjpassa@gmail.com>
2015-10-19 15:52:58 -0400

    created initial course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    instructor = models.CharField(max_length=100)
+    duration = models.IntegerField(choices=[(2, "2 Weeks"), (8, "8 Weeks")])
+    courseArt = models.ImageField()
+
+    def __str__(self):
+        return "{}: {}".format(self.title, self.instructor)
