4abdc407bf7aa0e80b89fd61e0f99d2efc0b7468
PJ <pjpassa@gmail.com>
2015-10-20 09:33:45 -0400

    redirected home to list view

--- DjangoCourseCatalog/urls.py
+++ DjangoCourseCatalog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url, patterns
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 from DjangoCourseCatalog import settings
 from courses import views
 
@@ -20,4 +22,6 @@ urlpatterns = patterns(
     url(r'^courses/(?P<pk>\d+)/change$',
         views.CourseAddChangeView.as_view(),
         name="change"),
+    url(r'^$',
+        RedirectView.as_view(url=reverse_lazy("list"))),
     ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
